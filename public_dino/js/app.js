6a8763cbdbbe928aa8bc10832d8ce39db5a850e2
Demy <demym@yahoo.it>
2017-04-11 12:13:38 +0200

    anonim02

--- public_dino/js/app.js
+++ public_dino/js/app.js
@@ -1,10 +1,10 @@
 var debug = false;
 
-var rooturl = "http://localhost:3000";
+//var rooturl = "http://localhost:3000";
 //var rooturl = "http://iotindustry40.mybluemix.net";
 //var rooturl = "http://gd-demo-dashboard.mybluemix.net";
 //var rooturl = "http://9.71.213.181:3000";
-//var rooturl = "https://ibm-industry4-dashboard.eu-gb.mybluemix.net";
+var rooturl = "https://ibm-industry4-dashboard.eu-gb.mybluemix.net";
 
 var linktoMaximo = false;
 var datatobeused = "23-09-2016"
